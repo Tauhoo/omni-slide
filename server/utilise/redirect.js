2b940cb12298ebb7d495c4209baf202c3dfe00b9
Tauhoo <tauhoo_ice@hotmail.com>
2019-11-05 19:41:22 +0000

    remove debug in redirect

--- server/utilise/redirect.js
+++ server/utilise/redirect.js
@@ -1,8 +1,6 @@
 const { client } = require("../config")
 const redirect = (req, res, next) => {
   try {
-    console.log(req.originalUrl)
-
     res.redirect(client + req.originalUrl)
   } catch (e) {
     res.status(500).send({ status: "error" })
