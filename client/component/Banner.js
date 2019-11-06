fcacdfc54e57dd654b4338cc734add44a06c2d45
wachirawit wacharak <tauhoo@wachirawits-MacBook-Pro.local>
2019-11-06 10:01:24 +0700

    update banner animation

--- client/component/Banner.js
+++ client/component/Banner.js
@@ -1,32 +1,16 @@
 import React from "react"
 import styled from "styled-components"
 import WebContainer from "./WebContainer"
-import BannerImage from "../static/banner.svg"
 const Container = styled.div`
   width: 100%;
-  height: 500px;
+  padding: 40px;
   background-color: #2c3e50;
   display: flex;
   justify-content: center;
   align-items: center;
   overflow: hidden;
-
-  @media (max-width: 650px) {
-    height: 400px;
-  }
-
-  @media (max-width: 500px) {
-    height: 300px;
-  }
-`
-
-const BannerImageStyled = styled(BannerImage)`
-  font-size: 550px;
-  @media (max-width: 650px) {
-    font-size: 400px;
-  }
-  @media (max-width: 500px) {
-    font-size: 300px;
+  @media (max-width: 700px) {
+    padding: 20px;
   }
 `
 
@@ -37,11 +21,16 @@ const Wrapper = styled.div`
   background-color: #2c3e50;
 `
 
+const Image = styled.img`
+  height: 300px;
+  max-width: 100%;
+`
+
 export default () => (
   <Wrapper>
     <WebContainer>
       <Container>
-        <BannerImageStyled></BannerImageStyled>
+        <Image src='/static/banner.svg'></Image>
       </Container>
     </WebContainer>
   </Wrapper>
--- client/config.js
+++ client/config.js
@@ -1,5 +1,2 @@
-export const server_src =
-  process.env.NEXT_ENV === "production"
-    ? "https://omni-slide.tk:80"
-    : "http://localhost:3000"
+export const server_src = "https://omni-slide.tk:80"
 export const web_name = "omni-slide"
