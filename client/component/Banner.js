78db5705802021e0a2cf65f1a6c8cf2dd5c6dda6
wachirawit wacharak <tauhoo@wachirawits-MacBook-Pro.local>
2019-11-06 11:43:37 +0700

    remove container of image banner

--- client/component/Banner.js
+++ client/component/Banner.js
@@ -1,29 +1,6 @@
 import React from "react"
 import styled from "styled-components"
 import WebContainer from "./WebContainer"
-const Container = styled.div`
-  width: 100%;
-  padding: 40px;
-  height: 400px;
-  background-color: #2c3e50;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  overflow: hidden;
-  @media (max-width: 1060px) {
-    height: 300px;
-  }
-  @media (max-width: 900px) {
-    height: 250px;
-  }
-  @media (max-width: 670px) {
-    height: 200px;
-  }
-  @media (max-width: 520px) {
-    height: 150px;
-  }
-`
-
 const Wrapper = styled.div`
   width: 100%;
   display: flex;
@@ -32,15 +9,13 @@ const Wrapper = styled.div`
 `
 
 const Image = styled.img`
-  height: 100%;
+  width: 100%;
 `
 
 export default () => (
   <Wrapper>
     <WebContainer>
-      <Container>
-        <Image src='/static/banner.svg'></Image>
-      </Container>
+      <Image src='/static/banner.svg'></Image>
     </WebContainer>
   </Wrapper>
 )
